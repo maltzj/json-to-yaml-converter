bd012989d3c8ff27f79253f7376ab56dc58fb80f
Jonathan Maltz <jm1271@gmail.com>
2023-04-07 19:01:36 +0530

    Implement arrays.  Nested arrays aren't quite working yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate tempdir;
 
 use serde_json;
+use serde_json::Value;
 use std::error::Error;
 use std::fs::File;
 use std::io;
@@ -24,7 +25,7 @@ fn run(args: impl Iterator<Item = String>) -> Result<String, Box<dyn Error>> {
     Ok(String::from("TODO: Fill me in"))
 }
 
-fn deserialize(file_path: &str) -> Result<serde_json::Value, Box<dyn Error>> {
+fn deserialize(file_path: &str) -> Result<Value, Box<dyn Error>> {
     let file = File::open(file_path)?;
     let reader = io::BufReader::new(file);
 
@@ -38,8 +39,121 @@ fn open_file_for_writing(file_path: &str) -> Result<File, Box<dyn Error>> {
     Ok(opened_file)
 }
 
-fn convert_to_yaml_string(serde: serde_json::Value) -> String {
-    "".to_string()
+fn convert_to_yaml_string(serde: Value) -> String {
+    convert_to_yaml_string_internal(serde).trim().to_string()
+}
+
+fn convert_to_yaml_string_internal(serde: Value) -> String {
+    let mut result = String::from("");
+    match serde {
+        Value::Bool(value) => {
+            result.push_str(&format!("{}", value));
+        }
+        Value::Number(num) => {
+            result.push_str(&format!("{}", num));
+        }
+        Value::String(string) => {
+            if string.len() == 0 {
+                result.push_str("''")
+            } else {
+                result.push_str(&format!("{}", string));
+            }
+        }
+        Value::Array(vector) => {
+            for value in vector {
+                match value {
+                    Value::Bool(_) | Value::Number(_) | Value::String(_) => {
+                        let sub_result = convert_to_yaml_string_internal(value);
+                        result.push_str(&format!("-  {}", sub_result));
+                    }
+                    Value::Array(_) => {
+                        let sub_result = convert_to_yaml_string_internal(value);
+                        let mut final_result = String::from("");
+                        for row in sub_result.split("\n") {
+                            final_result.push_str(&format!("  {}", row));
+                        }
+
+                        final_result.insert_str(0, "-  {}");
+                    }
+                    _ => (),
+                }
+            }
+        }
+        _ => (),
+    }
+    result.push_str("\n");
+    result
+}
+
+// TODO: Do I want to write some test which round trips these two things and asserts that they're
+// equal?  Probably; for now I'll just test this way to get the basics!
+#[cfg(test)]
+mod parsing_tests {
+    use super::*;
+
+    #[test]
+    fn it_converts_boolean() {
+        let data = "true";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "true");
+    }
+
+    #[test]
+    fn it_converts_numbers() {
+        let data = "12";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "12");
+    }
+
+    #[test]
+    fn it_converts_strings() {
+        let data = "\"test\"";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "test");
+    }
+
+    #[test]
+    fn it_converts_empty_string() {
+        let data = "\"\"";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "''");
+    }
+
+    #[test]
+    fn it_converts_an_array_with_one_element() {
+        let data = "[1]";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "-  1");
+    }
+
+    #[test]
+    fn it_converts_an_array_with_two_element() {
+        let data = "[1, 2]";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "-  1\n-  2");
+    }
+
+    #[test]
+    fn it_converts_an_array_with_all_primtive_elements() {
+        let data = "[1, false, \"a potato\"]";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "-  1\n-  false\n-  a potato");
+    }
+
+    #[test]
+    fn it_converts_nested_arrays() {
+        let data = "[[1]]";
+        let result =
+            convert_to_yaml_string(serde_json::from_str(data).expect("Could not parse data"));
+        assert_eq!(result, "- - 1");
+    }
 }
 
 #[cfg(test)]
@@ -73,6 +187,28 @@ mod tests {
         writeln!(temp_file, "{{\\\"key\":}}").expect("write failed");
 
         let failed_deserialize = match path.into_os_string().into_string() {
+            Ok(val) => match deserialize(&val) {
+                Ok(_) => panic!("Should not have deserialized file"),
+                Err(_) => true,
+            },
+            Err(error) => panic!("Could not create path! {:?}", error),
+        };
+        assert!(failed_deserialize);
+    }
+
+    #[test]
+    fn it_fails_on_non_existent_file() {
+        let temp_dir_path = TempDir::new("directory").expect("couldn't create directory");
+        let path = temp_dir_path.path().join("test-file.json");
+        let mut temp_file = File::create(&path).expect("failed to create file");
+        writeln!(temp_file, "{{\"key\":}}").expect("write failed");
+
+        let failed_deserialize = match temp_dir_path
+            .path()
+            .join("dne")
+            .into_os_string()
+            .into_string()
+        {
             Ok(val) => match deserialize(&val) {
                 Ok(_) => panic!("Should not have deserialized file"),
                 Err(_) => true,
